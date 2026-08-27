e70e68934ee01432df4260c4d68f414c0b009270
rajeshbarange00-svg <rajeshbarange00@gmail.com>
2026-08-27 22:18:12 +0530

    feat: add admin route entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AdminGate from './admin/AdminGate.jsx'
+
+const isAdminRoute = window.location.pathname.startsWith('/admin')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isAdminRoute ? <AdminGate /> : <App />}
   </StrictMode>,
 )
